ac35938ff77ed085a775886f0d6cff6f8f1b5f55
Felix Binder <me@felixbinder.net>
2020-10-28 20:05:45 +0100

    Reading in all .csvs

--- read_data.R
+++ read_data.R
@@ -1,1 +1,5 @@
-games <- read.csv("data/games.csv")
+games <- read.csv("data/games.csv") 
+#everything we need is in games.csv, the other files are just data computed from
+guesses <- read.csv("data/guesses.csv")
+repeat_player_prediction <- read.csv("data/repeat-player-prediction.csv")
+higher_lower_data2 <- read.csv("data/higher-lower-data2.csv")
