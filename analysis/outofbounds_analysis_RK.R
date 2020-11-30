90cfd8b5707a5c857ee1b8d7d2737da8d03fe54f
robwoodforest <46532858+robwoodforest@users.noreply.github.com>
2020-11-30 14:51:45 -0800

    Update outofbounds_analysis_RK.R
    det up document and added questions to answer, no actual analysis yet

--- analysis/outofbounds_analysis_RK.R
+++ analysis/outofbounds_analysis_RK.R
@@ -1,33 +1,18 @@
+setwd("/Users/rkaufman/Documents/Github/number_guessing_game_PSYCH_201/data") #for Rob
 suppressMessages(library(tidyverse))
 library(nFactors)
 repeat_player_prediction <- read.csv('repeat-player-prediction.csv')
 games <- read.csv('games.csv')
-guesses <- read.csv('guesses.csv')
+guesses_processed <- read.csv('guesses_processed.csv')
 
-#What other strategies do players use and how can we characterize them? (Naomi, Felix)
-#Factor analysis of strategies for optimal strategy
-#build intuitions by printing out random non-optimal games
-
-#https://www.statmethods.net/advstats/factor.html
-
-#Average distance to last guess- each last guess is different, group by number of guesses first
-#average distance of correct answer to the last guess?
-games %>% ggplot(aes(x=numGuesses))+
-  geom_histogram(binwidth = 1)
-summary(games$numGuesses)
-group_by(games)
-
-# Information gain (not sure what this mean)
-
-#Potential strategies: Using previous result as a prior
-#binary search rounded to 10/5’s/evens/odds
-#Sequential
-#Random
-
-
-
-#format
-fit<- factanal(games$guess1:games$guess15, 3, rotation= "varimax")
-print(fit, digits = 2, cutoff= .3, sort= TRUE)
-load<- fit$loadings[,1:2]
-plot(load, type= "n")
+#'What's up with out of bounds guesses? When are they most likely to occur? 
+#'Are there any trends that precit when a guess will be out of bounds? (Rob)
+#'1. Find out if there are any incomplete games in the dataset (when the final response for a 
+#'game =! the target #)
+#'2. Are there any factors that predict when an OOB guess will occur? 
+#'A. # of guesses
+#'B. Game/guess speed
+#'C. From certain types of players
+#'3. Why do we think these are occurring?
+#'A. Forgetting?
+#'B. Poor players?
