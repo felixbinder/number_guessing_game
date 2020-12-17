3b9f01946a3d9a44e838901affb5cde4499d6ae2
camrobjones <crjones94@googlemail.com>
2020-12-17 19:42:43 +0000

    add empty plots

--- analysis/slides_plots.R
+++ analysis/slides_plots.R
@@ -29,6 +29,37 @@ users.random <- games.random %>%
     EIG.relative = mean(EIG.relative)
   )
 
+# Empty
+
+
+p.eig.user.empty <- users %>%
+  ggplot(aes(x = EIG)) + 
+  # geom_density(data = users.random,
+  #              color=color.random, fill=color.random, alpha=0.5, size=0.5) +
+  # geom_density(data = users.binary,
+  # color=color.binary, fill=color.binary, alpha=0.5, size=0.5) +
+  # geom_density(color=color.human, fill = color.human, alpha=0.8, size=0.5) +
+  geom_hline(yintercept=0, color="white", size=0.5) +
+  theme_minimal() +
+  labs(
+    x = "Expected Information Gain",
+    y = "Density"
+  ) + 
+  xlim(0, 1.5) +
+  ylim(0, 7) +
+  theme(
+    panel.background = element_rect(fill = "transparent",colour = NA),
+    plot.background = element_rect(fill = "transparent",colour = NA),
+    panel.grid = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.text = element_text(color = "white"),
+    text = element_text(color = "white", size=20)
+  )
+
+ggsave("eig_user_empty.png", p.eig.user.empty, bg = "transparent", width = 10, height = 5)
+
+
 # Random guessing
 
 p.eig.user.random <- users %>%
@@ -113,6 +144,45 @@ ggsave("eig_user.png", p.eig.user, bg = "transparent", width = 10, height = 5)
 
 # ----- REIG vs Game Index ----- #
 
+# Empty
+p.reig.idx.empty <- games %>%
+  filter(no_user_games > 2) %>%
+  ggplot(aes(game_index, EIG.relative)) +
+  # stat_summary_bin(fun.data="mean_cl_boot",
+  #                  geom="pointrange",
+  #                  binwidth = 5,
+  #                  color="transparent") +
+  # geom_smooth(method = 'lm', formula = y~x, color="transparent",fill="transparent") +
+  geom_hline(yintercept = guesses.random %>%
+               pull(EIG.relative) %>% mean(na.rm=T),
+             color = color.random,
+             size = 1,
+             linetype = "dashed"
+  )+
+  geom_hline(yintercept = guesses.binary %>%
+               pull(EIG.relative) %>% mean(na.rm=T),
+             color = color.binary,
+             size = 1,
+             linetype = "dashed"
+  )+
+  ylim(0.76, 1.0321930) +
+  xlim(1, 137.5) +
+  theme_minimal() + 
+  labs(
+    x = "No. previous games played by user",
+    y = "Relative Expected Information Gain"
+  ) + 
+  theme(
+    panel.background = element_rect(fill = "transparent",colour = NA),
+    plot.background = element_rect(fill = "transparent",colour = NA),
+    panel.grid = element_blank(),
+    axis.text = element_text(color = "white"),
+    text = element_text(color = "white", size=20)
+  )
+
+ggsave("reig_idx_empty.png", p.reig.idx.empty, bg = "transparent", width = 10, height = 5)
+
+
 # Overall
 p.reig.idx.1 <- games %>%
   filter(no_user_games > 2) %>%
@@ -192,6 +262,42 @@ ggsave("reig_idx_2.png", p.reig.idx.2, bg = "transparent", width = 10, height = 
 
 
 # REIG Relative to Participant Mean
+
+## Empty
+
+p.reig.idx.3.empty <- games %>%
+  mutate(
+    EIG.relative.self = EIG.relative / EIG.relative.user
+  ) %>%
+  filter(no_user_games > 2) %>%
+  ggplot(aes(game_index, EIG.relative.self)) +
+  # stat_summary_bin(fun.data="mean_cl_boot",
+  #                  geom="pointrange",
+  #                  binwidth = 5,
+  #                  color=color.human) +
+  geom_hline(yintercept = 1,
+             color = "white",
+             size = 1,
+             linetype = "dashed"
+  )+
+  # geom_smooth(method = 'lm', formula = y~x, color=color.human) + 
+  theme_minimal() + 
+  ylim(0.9, 1.05) + 
+  xlim(1, 137.5) +
+  labs(
+    x = "No. previous games played by user",
+    y = "REIG change (by user)"
+  ) + theme(
+    panel.background = element_rect(fill = "transparent",colour = NA),
+    plot.background = element_rect(fill = "transparent",colour = NA),
+    panel.grid = element_blank(),
+    axis.text = element_text(color = "white"),
+    text = element_text(color = "white", size=20),
+    legend.position = "none"
+  )
+
+ggsave("reig_idx_3_empty.png", p.reig.idx.3.empty, bg = "transparent", width = 10, height = 5)
+
 
 games <- merge(games, 
                users %>% select(user, EIG.relative) %>%
