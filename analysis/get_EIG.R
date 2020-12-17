6bb1f38e1726717bbbea2839c50af66db6a05e80
Felix Binder <me@felixbinder.net>
2020-12-17 16:41:04 +0100

    Commented entropy calculation

--- analysis/get_EIG.R
+++ analysis/get_EIG.R
@@ -1,11 +1,12 @@
 get_EIG<- function(lower, upper, guess) {
-  k.cur = upper-lower+1
-  h.cur = log2(k.cur)
+  k.cur = upper-lower+1 #number of available candidates
+  h.cur = log2(k.cur) #entropy over available options (collapses from entropy calc in the case of equally likely options)
+  #\Eta(X) = -\sum_{i=1}^n {\mathrm{P}(x_i) \log \mathrm{P}(x_i)}
   
   # Info if greater
-  k.greater = upper-guess
-  p.greater = k.greater / k.cur
-  h.greater = ifelse(p.greater > 0, p.greater * log2(k.greater), 0)
+  k.greater = upper-guess #upper range
+  p.greater = k.greater / k.cur #probability of target being in upper range
+  h.greater = ifelse(p.greater > 0, p.greater * log2(k.greater), 0) #weighted entropy
   
   # Info if lower
   k.lower = guess-lower
@@ -20,5 +21,5 @@ get_EIG<- function(lower, upper, guess) {
   # Sum
   h.next  = h.lower + h.greater + h.correct
   
-  h.cur - h.next
+  h.cur - h.next #return expected information gain (old entropy minus new entropy for the three possible outcomes)
 }
