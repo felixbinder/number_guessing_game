bee9f4cfe16820616a940bbbafadb28efce951a1
Felix Binder <me@felixbinder.net>
2020-12-17 15:21:20 +0100

    Bunch of small fixes and cleanup

--- read_data.R
+++ read_data.R
@@ -1,3 +1,5 @@
+#Use data/process_data.Rmd to process the data if needed
+
 #read in preprocessed csvs. Working directory should be the top folder of the repo
 games <- read.csv("data/games_processed.csv") 
 #everything we need is in games.csv, the other files are just data computed from
